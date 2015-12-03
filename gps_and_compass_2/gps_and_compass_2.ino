84219eea30ff2423f92bf1d6d3749d775ca8790e
neilmanuell <neil.revisual@gmail.com>
2015-12-03 10:39:24 +0000

    Angle taking now working
    hardcoded gps to 180 for testing

--- gps_and_compass_2/gps_and_compass_2.ino
+++ gps_and_compass_2/gps_and_compass_2.ino
@@ -2,89 +2,96 @@
 #include <TinyGPS.h>
 
 TinyGPS tiny_gps;
-SoftwareSerial gps = SoftwareSerial(3,4);
+SoftwareSerial gps = SoftwareSerial(3, 4);
 
 static void gps_smartdelay(unsigned long ms);
-static void print_float(float val, float invalid, int len, int prec);
-static void print_int(unsigned long val, unsigned long invalid, int len);
-static void print_date(TinyGPS &tiny_gps);
-static void print_str(const char *str, int len);
 
-#define CMPS_GET_ANGLE8 0x12
 #define CMPS_GET_ANGLE16 0x13
+//static const CMPS_GET_ANGLE16 0x13
 
-enum GPS_STATE {
-  NONE,
-  WRITING,
-  READING 
+static const int BEATING_DELAYS[] = {10, 150, 10, 1500};
+static const int PANIC_DELAYS[] = {10, 10, 10, 10};
+
+enum HEART_STATE {
+  RESTING,
+  BEATING,
+  PANIC
 };
 
-static GPS_STATE gps_state = NONE;
+static HEART_STATE heart_state = RESTING;
+
+enum GPS_STATE {
+  RESET,
+  WRITING,
+  READING
+};
+
+static GPS_STATE gps_state = RESET;
 static unsigned long gps_start;
 static const unsigned long gps_delay = 1000;
+static int solenoidPin = 9;
 
-SoftwareSerial cmps11 = SoftwareSerial(6,5);
+static int gps_heading = 0;
+
+static const double TARGET_LAT = 51.508131, TARGET_LON = -0.128002;
+static const int TARGET_RANGE_TIGHT = 5; // margin of error for within tight range of target
+static const int TARGET_RANGE_LOOSE = 45; // margin of error for within loose range of target
+
+SoftwareSerial cmps11 = SoftwareSerial(6, 5);
 unsigned char high_byte, low_byte, angle8;
 unsigned int angle16;
 unsigned int current_angle;
 
 void setup()
 {
-  Serial.begin(115200);  
+  Serial.begin(115200);
   gps.begin(9600);
   cmps11.begin(9600);
+  pinMode(solenoidPin, OUTPUT);
 }
 
 void loop()
 {
-  doGPS();  
+  doGPS();
   doCompass();
   calculateAngle();
-  doBeat(); 
+  doBeat();
 }
 
 void doGPS()
 {
   gps.listen();
 
-  if( gps_state == NONE)
+  if ( gps_state == RESET)
   {
     gps_start = millis();
     gps_state = READING;
   }
 
-  if( gps_state == READING)
+  if ( gps_state == READING)
   {
-    gps_smartdelay(gps_delay); 
+    gps_smartdelay(gps_delay);
   }
 
   else if (gps_state == WRITING)
-  {     
-    getGPSData();
-   // gps_smartdelay(); 
-    gps_state = NONE;
+  {
+    applyGPSData();
+    gps_state = RESET;
   }
-  
+
 }
 
-void getGPSData()
+void applyGPSData()
 {
-   float flat, flon;
-  unsigned long age, date, time, chars = 0;
-  unsigned short sentences = 0, failed = 0;
-  static const double TARGET_LAT = 51.508131, TARGET_LON = -0.128002;
-   tiny_gps.f_get_position(&flat, &flon, &age);
-   Serial.print("GPS::");
-   Serial.println();
-   print_str(tiny_gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(tiny_gps.f_course()), 6);
-   print_int(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0xFFFFFFFF : (unsigned long)TinyGPS::distance_between(flat, flon, TARGET_LAT, TARGET_LON) / 1000, 0xFFFFFFFF, 9);
-   print_float(flat == TinyGPS::GPS_INVALID_F_ANGLE ? TinyGPS::GPS_INVALID_F_ANGLE : TinyGPS::course_to(flat, flon, TARGET_LAT, TARGET_LON), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-   print_str(flat == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(TinyGPS::course_to(flat, flon, TARGET_LAT, TARGET_LON)), 6);
- 
-   tiny_gps.stats(&chars, &sentences, &failed);
-   print_int(chars, 0xFFFFFFFF, 6);
-  
-   Serial.println();
+  float flat, flon;
+  unsigned long age;
+
+  tiny_gps.f_get_position(&flat, &flon, &age);
+  gps_heading = 180;//(int)tiny_gps.course_to(flat, flon, TARGET_LAT, TARGET_LON);
+  Serial.print("gps_heading = " + gps_heading);
+  Serial.println();
+
+
 }
 
 void doCompass()
@@ -92,128 +99,97 @@ void doCompass()
   unsigned int new_angle;
   cmps11.listen();
   cmps11.write(CMPS_GET_ANGLE16);  // Request and read 16 bit angle
-  while(cmps11.available() < 2);
+  while (cmps11.available() < 2);
   high_byte = cmps11.read();
   low_byte = cmps11.read();
   angle16 = high_byte;                // Calculate 16 bit angle
   angle16 <<= 8;
-  angle16 += low_byte;  
+  angle16 += low_byte;
 
   new_angle = angle16 / 10;
 
-  if( new_angle != current_angle)
+  if ( new_angle != current_angle)
   {
     current_angle = new_angle;
-    Serial.print(" cmps11   angle full: ");       // Display 16 bit angle with decimal place
-  
-    Serial.print(current_angle, DEC);
-    Serial.print(".");
-    Serial.print(angle16 % 10, DEC);
+    //Serial.print(" cmps11   angle full: ");       // Display 16 bit angle with decimal place
 
-    Serial.println();
+    //Serial.print(current_angle, DEC);
+   
+
+    //Serial.println();
   }
 
- 
+
 }
 
 void calculateAngle()
 {
-  
+  int headingsDiff = abs(gps_heading - current_angle);
+
+  if (headingsDiff <= TARGET_RANGE_TIGHT)
+  {
+    //if your heading is close enough to the target heading, then make something happen
+    if ( heart_state != PANIC )
+    {
+      Serial.println("THAT WAY!");
+      heart_state = PANIC;
+    }
+
+  }
+
+  else  if (headingsDiff <= TARGET_RANGE_LOOSE)
+  {
+    if ( heart_state != BEATING )
+    {
+      Serial.println("WARM");
+      heart_state = BEATING;
+    }
+
+  }
+
+  else if (headingsDiff > TARGET_RANGE_LOOSE)
+  {
+    if ( heart_state != RESTING )
+
+    { // you're still off target
+
+      Serial.println("NOTHING");
+      heart_state = RESTING;
+    }
+  }
 }
 
 void doBeat()
 {
-  
+  if ( heart_state == PANIC )
+  {
+
+  }
+
+  else if ( heart_state == BEATING )
+  {
+
+  }
+
+  else if ( heart_state == RESTING )
+  {
+
+  }
 }
 
-/*static void gps_smartdelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do 
-  {
-    while (gps.available())
-      tiny_gps.encode(gps.read());
-  } while (millis() - start < ms);
-   gps_state = WRITING;
-}*/
-
 static void gps_smartdelay(unsigned long ms)
-{ 
-
- 
+{
   while (gps.available())
   {
-     tiny_gps.encode(gps.read());
-
+    tiny_gps.encode(gps.read());
   }
 
   if ( (millis() - gps_start) > gps_delay)
   {
     gps_state = WRITING;
-  }     
- 
-}
-
-
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
   }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  gps_smartdelay(0);
+
 }
 
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-  char sz[32];
-  if (val == invalid)
-    strcpy(sz, "*******");
-  else
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  gps_smartdelay(0);
-}
 
-static void print_date(TinyGPS &tiny_gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  tiny_gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  gps_smartdelay(0);
-}
 
-static void print_str(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  gps_smartdelay(0);
-}
