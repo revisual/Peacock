193b772143d3ac213d78959eb719166c3c18ba55
neilmanuell <neil.revisual@gmail.com>
2015-12-05 12:35:10 +0000

    adds distance check.

--- peacock/peacock.ino
+++ peacock/peacock.ino
@@ -6,25 +6,26 @@
 
 static const int BEAT_PIN = 9;
 
-static const double TARGET_LAT = 50.853683 , TARGET_LON = -0.117813;
+static const double TARGET_LAT = 50.842537 , TARGET_LON = -0.137835;
 static const int TARGET_RANGE_TIGHT = 5; // margin of error for within tight range of target
 static const int TARGET_RANGE_LOOSE = 45; // margin of error for within loose range of target
+static const int TARGET_DISTANCE = 5; // margin of error for within loose range of target
 
 SoftwareSerial ss_gps1 = SoftwareSerial(3, 4);
 TidyGPS gps(&ss_gps1);
 
 SoftwareSerial ss_cmps11 = SoftwareSerial(6, 5);
 HeadingCMPS11 cmps11(&ss_cmps11);
-unsigned int current_angle;
 
+Beat beat(BEAT_PIN);
 enum beatType {
   NONE,
   STEADY,
-  RAPID
+  RAPID,
+  CONSTANT
 };
 
 beatType currentBeat = NONE;
-Beat beat(BEAT_PIN);
 
 void setup()
 {
@@ -32,23 +33,29 @@ void setup()
   gps.begin(9600);
   gps.setTargetCoords(TARGET_LAT, TARGET_LON );
   cmps11.begin(9600);
-
 }
 
 void loop()
 {
   gps.advance();
   beat.advance();
-  current_angle = cmps11.getHeading();
-
-  calculateAngle();
-  //doBeat();
+  setBearing(cmps11.getHeading());
 }
 
 
-void calculateAngle()
+void setBearing(unsigned int current_angle)
 {
-  if ( gps.isBearingWithinTolerance( current_angle, 5 ))
+  if ( gps.isDistanceWithinTolerance( TARGET_DISTANCE ))
+  { 
+    if ( currentBeat != CONSTANT )
+    {
+      currentBeat = CONSTANT;
+      beat.stop();
+      beat.on();
+    }
+  }
+  
+  else if ( gps.isBearingWithinTolerance( current_angle, TARGET_RANGE_TIGHT ))
   {
  
     if ( currentBeat != RAPID )
@@ -60,20 +67,18 @@ void calculateAngle()
 
   }
 
- else if ( gps.isBearingWithinTolerance( current_angle, 45 ))
+ else if ( gps.isBearingWithinTolerance( current_angle, TARGET_RANGE_LOOSE ))
   {
-    if ( currentBeat != STEADY)
-    {
+    if ( currentBeat != STEADY)    {
       currentBeat = STEADY;
       beat.stop();
-      beat.start(20, 200, 20, 2000);
+      beat.start(20, 200, 20, 1500);
     }
 
   }
 
   else if (  currentBeat != NONE )
   {
-    Serial.println("NONE");
     currentBeat = NONE;
     beat.stop();
   }
